93f8ed516caae8006e2611a22c35873b9b918dbd
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-07-21 14:49:57 +0900

    maint: multithread setup

--- setup.sh
+++ setup.sh
@@ -5,11 +5,20 @@ pip install uv
 uv pip install --system -r requirements.txt -r examples/requirements.txt
 uv pip install --system 'gdown<6.0.0'
 
-mkdir -p ./_data/v1/profiles
-gdown --fuzzy "$PROFILES_V1_GDRIVE" -O ./_data/v1/profiles.tar
-tar -xf _data/v1/profiles.tar -C _data/v1/profiles
-mv _data/v1/profiles/SlurryProperties _data/v1/profiles/SlurryViscosities _data/v1/
+mkdir -p ./_data/v1/profiles ./_data/v1/ca
 
-mkdir -p ./_data/v1/ca
-gdown --fuzzy "$CA_V1_GDRIVE" -O ./_data/v1/ca.tar
-tar -xf _data/v1/ca.tar -C _data/v1/ca
+(
+  gdown --fuzzy "$PROFILES_V1_GDRIVE" -O ./_data/v1/profiles.tar
+  tar -xf _data/v1/profiles.tar -C _data/v1/profiles
+  mv _data/v1/profiles/SlurryProperties _data/v1/profiles/SlurryViscosities _data/v1/
+) &
+profiles_pid=$!
+
+(
+  gdown --fuzzy "$CA_V1_GDRIVE" -O ./_data/v1/ca.tar
+  tar -xf _data/v1/ca.tar -C _data/v1/ca
+) &
+ca_pid=$!
+
+wait "$profiles_pid"
+wait "$ca_pid"
